00de14a70694a47442a4e2738dab56c6a4292118
JurIVoelker <juri.voelker@gmx.de>
2025-03-25 18:36:54 +0100

    fix: wrap login card in suspense

--- server/app/login/page.tsx
+++ server/app/login/page.tsx
@@ -1,10 +1,18 @@
 import CenteredWrapper from "@/components/centered-wrapper";
 import LoginCard from "@/components/pages/login-card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Suspense } from "react";
 
 const LoginPage = () => {
   return (
     <CenteredWrapper>
-      <LoginCard />
+      <Suspense
+        fallback={
+          <Skeleton className="max-w-md w-[calc(100%-2rem)] min-w-[200px] h-58" />
+        }
+      >
+        <LoginCard />
+      </Suspense>
     </CenteredWrapper>
   );
 };
