4cc25c9d8db1d27cef93b93c62de1b3ac6c0aceb
JurIVoelker <Juri.Voelker@gmx.de>
2025-03-29 11:16:12 +0100

    fix: redirect to dashboard

--- server/middleware.ts
+++ server/middleware.ts
@@ -5,19 +5,28 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
 
+  // Public paths that don't require authentication
   if (path === "/login") {
     return NextResponse.next();
   }
 
+  // Check authentication for all other paths
   const cookies = request.cookies;
   const password = cookies.get("password");
 
   const { USER_SECRET } = process.env;
 
   if (password?.value === USER_SECRET) {
+    // User is authenticated, allow the request to proceed
+
+    if (path === "/" || path === "") {
+      return NextResponse.redirect(new URL(`dashboard`, request.url));
+    }
+
     return NextResponse.next();
   }
 
+  // User is not authenticated, redirect to login page
   const response = NextResponse.redirect(
     new URL(`/login?redirect=${path}`, request.url)
   );
@@ -25,10 +34,6 @@ export function middleware(request: NextRequest) {
   response.cookies.set("password", "", {
     expires: new Date(0),
   });
-
-  if (path === "/") {
-    return NextResponse.redirect(new URL(`dashboard`, request.url));
-  }
 
   return response;
 }
