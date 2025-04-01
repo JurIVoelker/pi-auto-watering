9986065626810128548d366c3d593e7394631a48
JurIVoelker <juri.voelker@gmx.de>
2025-04-01 19:49:30 +0200

    fix: improve error handling for API requests in weight measurement and upload functions

--- raspberry-pi/testing/rest.py
+++ raspberry-pi/testing/rest.py
@@ -9,10 +9,13 @@ def post_measure_weight(values):
   body = {
     "values": values,
   }
-  r = requests.post(url = f"{SERVER_URL}/api/measure/weight", headers=auth_header, json=body)
-  if r.status_code != 200:
+  r = requests.post(url=f"{SERVER_URL}/api/measure/weight", headers=auth_header, json=body)
+  if r.status_code == 500:
     print(f"Error: {r.status_code}: {r.text}")
     exit(1)
+  elif r.status_code != 200:
+    print(f"Log: {r.status_code}: {r.text}")
+    return None
   data = r.json()
   return data
 
@@ -27,26 +30,27 @@ def post_upload(filename, capturedAt):
     "height": image_width,
   }
   
-  r = requests.post(url = f"{SERVER_URL}/api/upload", headers=auth_header, json=body)
-  if r.status_code != 200:
+  r = requests.post(url=f"{SERVER_URL}/api/upload", headers=auth_header, json=body)
+  if r.status_code == 500:
     print(f"Error: {r.status_code}: {r.text}")
     exit(1)
+  elif r.status_code != 200:
+    print(f"Log: {r.status_code}: {r.text}")
+    return None
   data = r.json()
   return data
 
 def get_sync():
   try:
-    r = requests.get(url = f"{SERVER_URL}/api/sync", headers=auth_header)
+    r = requests.get(url=f"{SERVER_URL}/api/sync", headers=auth_header)
   except requests.exceptions.RequestException as e:
     print("Connection Error")
     return None
-  if r.status_code != 200:
+  if r.status_code == 500:
     print(f"Error: {r.status_code}: {r.text}")
     exit(1)
+  elif r.status_code != 200:
+    print(f"Log: {r.status_code}: {r.text}")
+    return None
   data = r.json()
   return data
-
-
-# print(get_current_time())
-
-# res = post_upload("./camera-data/test-image.jpg", get_current_time())
