7296a38aa51ae0531af12851c91f8505a0a82252
JurIVoelker <juri.voelker@gmx.de>
2025-04-01 18:33:20 +0200

    fix: rotate image

--- raspberry-pi/testing/camera_testing.py
+++ raspberry-pi/testing/camera_testing.py
@@ -2,6 +2,7 @@ import os
 from picamera2 import Picamera2
 from utils import image_width, image_height
 from datetime import datetime
+from PIL import Image
 
 # Define the image folder
 image_folder = "camera-data"
@@ -20,5 +21,11 @@ def capture_image():
     picam.start()
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     filename = f"image_{timestamp}.jpg"
-    picam.capture_file(os.path.join(image_folder, filename))
+    filepath = os.path.join(image_folder, filename)
+    picam.capture_file(filepath)
     picam.stop()
+
+    # Rotate the image to the right (90 degrees clockwise)
+    with Image.open(filepath) as img:
+        rotated_img = img.rotate(-90, expand=True)
+        rotated_img.save(filepath)
