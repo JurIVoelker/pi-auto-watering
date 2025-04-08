a5b30f0d1c14144b8e111851d2679f5a35735376
JurIVoelker <juri.voelker@gmx.de>
2025-04-08 10:07:40 +0200

    fix: reinitialize camera on each capture

--- raspberry-pi/testing/camera_testing.py
+++ raspberry-pi/testing/camera_testing.py
@@ -1,33 +1,41 @@
 import os
-from picamera2 import Picamera2
 from utils import image_width, image_height
+from picamera2 import Picamera2
+import time
 from datetime import datetime
 from PIL import Image
-import time
+import os
 
 # Define the image folder
 image_folder = "camera-data"
+picam = Picamera2()
 
 # Create the folder if it doesn't exist
 if not os.path.exists(image_folder):
     os.makedirs(image_folder)
 
-width = image_width
-height = image_height
-picam = Picamera2()
-picam.configure(picam.create_still_configuration(main={"size": (width, height)}))
-picam.set_controls({"AeEnable": True})
-
 def capture_image():
-    picam.start()
+    width = image_width
+    height = image_height
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     filename = f"image_{timestamp}.jpg"
     filepath = os.path.join(image_folder, filename)
-    picam.capture_file(filepath)
-    picam.stop()
 
-    # Rotate the image to the right (90 degrees clockwise)
+    if picam.started:
+        picam.stop()
+    try:
+        picam.configure(picam.create_still_configuration(main={"size": (width, height)}))
+        picam.set_controls({"AeEnable": True, "AwbEnable": True, "AwbMode": "auto"})
+        picam.start()
+        time.sleep(1.5)  # Let AE settle
+        picam.capture_file(filepath)
+    finally:
+        # Ensure the camera is always properly stopped
+        picam.stop()
+
+    # Rotate image
     with Image.open(filepath) as img:
         rotated_img = img.rotate(-90, expand=True)
-        rotated_img.save(filepath)  
+        rotated_img.save(filepath)
+
     return filepath
