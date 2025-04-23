cb01fc72f249efab8739bd043d399c55e1107b14
JurIVoelker <juri.voelker@gmx.de>
2025-04-23 21:51:02 +0200

    fix: update testDatabase script to write measurements to JSON file

--- server/prisma/scripts/testDatabase.ts
+++ server/prisma/scripts/testDatabase.ts
@@ -1,13 +1,16 @@
 import { prisma } from "../prisma";
+import { writeFile } from "fs/promises";
+import { join } from "path";
 
 const exec = async () => {
   const mesaurements = await prisma.weightMeasurement.findMany({
-    take: 100,
     orderBy: {
       measuredAt: "desc",
     },
   });
-  console.log(mesaurements);
+  const filePath = join(__dirname, "measurements.json");
+  await writeFile(filePath, JSON.stringify(mesaurements, null, 2), "utf-8");
+  console.log(`Data written to ${filePath}`);
 };
 
 exec();
