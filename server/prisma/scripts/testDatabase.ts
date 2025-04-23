74d5f2909292104e3a65aa0c48b6e87155795251
JurIVoelker <juri.voelker@gmx.de>
2025-04-23 22:06:55 +0200

    fix: integrate Minio client for uploading measurements to bucket

--- server/prisma/scripts/testDatabase.ts
+++ server/prisma/scripts/testDatabase.ts
@@ -1,6 +1,18 @@
 import { prisma } from "../prisma";
 import { writeFile } from "fs/promises";
 import { join } from "path";
+import * as Minio from "minio";
+
+const { BUCKET_NAME, ENDPOINT, ACCESS_KEY, SECRET_KEY } = process.env;
+
+const bucketName = BUCKET_NAME || "undefined";
+
+const minio = new Minio.Client({
+  endPoint: ENDPOINT || "undefined",
+  useSSL: true,
+  accessKey: ACCESS_KEY || "undefined",
+  secretKey: SECRET_KEY || "undefined",
+});
 
 const exec = async () => {
   const mesaurements = await prisma.weightMeasurement.findMany({
@@ -11,6 +23,11 @@ const exec = async () => {
   const filePath = join(__dirname, "measurements.json");
   await writeFile(filePath, JSON.stringify(mesaurements, null, 2), "utf-8");
   console.log(`Data written to ${filePath}`);
+
+  await minio.fPutObject(bucketName, "measurements.json", filePath);
+  console.log(
+    `File uploaded to Minio bucket "${bucketName}" as "measurements.json"`
+  );
 };
 
 exec();
